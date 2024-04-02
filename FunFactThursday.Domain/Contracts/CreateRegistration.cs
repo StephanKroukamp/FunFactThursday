24b19b49eba77174de68f36cbaee69aa981c301c
stephankroukamp <stephankr@webuycars.co.za>
2024-04-02 17:18:31 +0200

    remove outbox working and update README.md

--- FunFactThursday.Domain/Contracts/CreateRegistration.cs
+++ FunFactThursday.Domain/Contracts/CreateRegistration.cs
@@ -3,8 +3,6 @@ namespace FunFactThursday.Domain.Contracts;
 public record CreateRegistration
 {
     public string EventName { get; set; } = null!;
-    
     public string UserEmailAddress { get; set; } = null!;
-    
     public decimal Payment { get; set; }
 }
